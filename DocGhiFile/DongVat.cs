a5e59b54cd01758fedc652b23825ef89b8c70cd8
shanghaik168@gmail.com <Shanghaik1998>
2024-01-30 20:56:40 +0700

    docjgi#1

---
+++ DocGhiFile/DongVat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocGhiFile
+{
+    internal class DongVat
+    {
+        public string Ten { get; set; }
+        public string MoTa { get; set; }
+        public int SoChan { get; set; }
+        public DongVat()
+        {
+        }
+        public DongVat(string ten, string moTa, int soChan)
+        {
+            Ten = ten;
+            MoTa = moTa;
+            SoChan = soChan;
+        }
+        public void InThongTin()
+        {
+            Console.WriteLine($"Tên: {Ten}, Mô tả: {MoTa} có số chân {SoChan}");
+        }
+        
+    }
+}
