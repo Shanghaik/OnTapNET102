a46134b21a07894e2f96179913cf1151a10d51af
shanghaik168@gmail.com <Shanghaik1998>
2024-01-30 21:26:28 +0700

    xml

--- DocGhiFile/Program.cs
+++ DocGhiFile/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            List<DongVat> dvs = new List<DongVat>()
+            {
+                new DongVat() {Ten = "Hổ", MoTa = "Ăn thịt", SoChan = 4},
+                new DongVat() {Ten = "Báo", MoTa = "Ăn thịt", SoChan = 4},
+                new DongVat() {Ten = "Cáo", MoTa = "Ăn quả", SoChan = 4},
+                new DongVat() {Ten = "Chồn", MoTa = "Ăn hạt dẻ", SoChan = 4},
+                new DongVat() {Ten = "Gà", MoTa = "Ăn thóc", SoChan = 2}
+            };
+            // ghi vào file
+             TextServices.WriteDataToFile(@"dongvat.txt", dvs);
+            // đọc file
+            List<DongVat> dvs2 = TextServices.ReadFromFile(@"dongvat.txt");
+            foreach (var item in dvs2)
+            {
+                item.InThongTin();
+            }
         }
     }
 }
--- DocGhiFile/TextServices.cs
+++ DocGhiFile/TextServices.cs
@@ -6,7 +6,70 @@ using System.Threading.Tasks;
 
 namespace DocGhiFile
 {
-    internal class TextServices
+    internal static class TextServices
     {
+        // Lớp static sẽ chứa các phương thức static, phương thức static là 
+        // phương thức có thể gọi trực tiếp thông qua tên class mà không cần tạo
+        // ra đối tượng cự thể
+        public static string ObjToString(DongVat dv)
+        {
+            return $"{dv.Ten}|{dv.MoTa}|{dv.SoChan}";
+        }// trả về 1 chuỗi chứa đủ thông tin chua đối tượng, mỗi thuộc tính sẽ được
+         // phân tách với thuộc tính tiếp theo bởi 1 dấu |
+
+        public static DongVat StringToObj(string data)
+        {
+            // Kiểm tra xem string có chứa 2 dấu | hay không?
+            if (data.Length - data.Replace("|", "").Length != 2)
+            {
+                return null;
+            }
+            else
+            {
+                // cắt chuỗi ra thành các phần chứa thông tin của đối tượng
+                string[] dataObj = data.Split('|'); // Cắt thành 1 mảng chứa dữ liệu của các
+                // thuộc tính
+                DongVat dv = new DongVat();
+                dv.Ten = dataObj[0]; // Tên chính là phần tử đầu tiên của mảng dataObj
+                dv.MoTa = dataObj[1];
+                dv.SoChan = Convert.ToInt32(dataObj[2]);
+                return dv;
+            }
+        }
+        // Đọc dữ liệu từ file dựa vào đường dẫn, trả về danh sách đối tượng DongVat
+        public static List<DongVat> ReadFromFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                List<DongVat> dvs = new List<DongVat>();
+                string[] files = File.ReadAllLines(path); // đọc tất cả các dòng từ file
+                foreach (var item in files)
+                {
+                    if (StringToObj(item) != null) // Nếu đọc được dữ liệu từ 1 dòng bất kì của file
+                    {
+                        dvs.Add(StringToObj(item));
+                    }
+                }
+                return dvs;
+            }
+            else { Console.WriteLine("File không tồn tại"); return null; }
+        }
+        public static void WriteDataToFile(string path, List<DongVat> dvs)
+        {
+            if(!File.Exists(path))
+            {
+                Console.WriteLine("File không tòn tại");
+            }
+            else
+            {
+                File.WriteAllText(path, ""); // Ghi đè một chuỗi rỗng vào file => Dữ liệu mất trắng
+                foreach (var item in dvs)
+                {
+                    string data = ObjToString(item);
+                    File.AppendAllText(path,("\n"+data)); // \n để xuống dòng cho mỗi bản ghi
+                }
+                Console.WriteLine("Ghi file thành công");
+            }
+        }
     }
 }
